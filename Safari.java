69f4efd0dbb4cbdadb2426f7335e29636f4a2d8d
BHASVIC-TanayVarghese25 <tanay.varghese25@students.bhasvic.ac.uk>
2026-07-13 10:16:39 +0100

    Delete Safari.java

--- Safari.java
+++
@@ -1,9 +0,0 @@
-public class Safari extends Animals{
-    Safari(int pPrice, int pLevel, int Prent, boolean pAnimal_owned, int pAnimal_owner) {
-        super(pPrice,pLevel,Prent,pAnimal_owned,pAnimal_owner);
-        String type = "Safari";
-
-    }
-
-
-}
