d2f93b1a384298a4c3d767abc65700c33e9c2415
BHASVIC-TanayVarghese25 <tanay.varghese25@students.bhasvic.ac.uk>
2026-07-13 10:15:15 +0100

    update animals

--- Animals.java
+++ Animals.java
@@ -2,15 +2,52 @@ public class Animals {
     int price;
     int level;
     int rent;
-    boolean animal_owned = false;
-    int animal_owner;
+    int animal_owner = 0;
+    String type;
+    String name;
 
-    Animals(int pPrice, int pLevel, int pRent, boolean pAnimal_owned, int pAnimal_owner){
+    Animals(int pPrice, int pLevel, int pRent, int pAnimal_owner, String pType, String pName){
         price = pPrice;
         level = pLevel;
         rent = pRent;
-        animal_owned = pAnimal_owned;
         animal_owner = pAnimal_owner;
+        type = pType;
+        name = pName;
+    }
 
+    public int getPrice() {
+        return price;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+    
+    public void setLevel(int pLevel) {
+        level = pLevel;
+    }
+
+    public int getRent() {
+        return rent;
+    }
+    
+    public void setRent(int pRent) {
+        rent = pRent;
+    }
+
+    public int getAnimal_owner() {
+        return animal_owner;
+    }
+    
+    public void setAnimal_owner(int pOwner) {
+        animal_owner = pOwner;
+    }
+
+    public String getType() {
+        return type;
+    }
+    
+    public String getName() {
+        return name;
     }
 }
