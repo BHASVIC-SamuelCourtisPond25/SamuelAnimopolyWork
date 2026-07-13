e9a31c57abf7e970b648be4196fb5d9602a57bc7
BHASVIC-TanayVarghese25 <tanay.varghese25@students.bhasvic.ac.uk>
2026-07-13 10:16:08 +0100

    Delete House.java

--- House.java
+++
@@ -1,6 +0,0 @@
-public class House extends Animals{
-    House(int pPrice, int pLevel, int Prent, boolean pAnimal_owned, int pAnimal_owner) {
-        super(pPrice,pLevel,Prent,pAnimal_owned,pAnimal_owner);
-        String type = "House";
-    }
-}
