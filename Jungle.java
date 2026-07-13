56de5d8394f645e009a313c734bca9d89a6594c4
BHASVIC-TanayVarghese25 <tanay.varghese25@students.bhasvic.ac.uk>
2026-07-13 10:16:15 +0100

    Delete Jungle.java

--- Jungle.java
+++
@@ -1,7 +0,0 @@
-public class Jungle extends Animals{
-
-    Jungle(int pPrice, int pLevel, int Prent, boolean pAnimal_owned, int pAnimal_owner) {
-        super(pPrice,pLevel,Prent,pAnimal_owned,pAnimal_owner);
-        String type = "Jungle";
-    }
-}
