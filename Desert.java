2ca450dd617ffff3da06ba4acfc1974beb6d4568
BHASVIC-TanayVarghese25 <tanay.varghese25@students.bhasvic.ac.uk>
2026-07-13 10:15:51 +0100

    Delete Desert.java

--- Desert.java
+++
@@ -1,7 +0,0 @@
-public class Desert extends Animals{
-
-    Desert(int pPrice, int pLevel, int Prent, boolean pAnimal_owned, int pAnimal_owner) {
-        super(pPrice,pLevel,Prent,pAnimal_owned,pAnimal_owner);
-        String type = "Desert";
-    }
-}
