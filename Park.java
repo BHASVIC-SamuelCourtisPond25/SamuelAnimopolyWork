4aa5bde82aec91faaa3018b9eb7f9a5d1f15fb01
BHASVIC-TanayVarghese25 <tanay.varghese25@students.bhasvic.ac.uk>
2026-07-13 10:16:31 +0100

    Delete Park.java

--- Park.java
+++
@@ -1,6 +0,0 @@
-public class Park extends Animals{
-    Park(int pPrice, int pLevel, int Prent, boolean pAnimal_owned, int pAnimal_owner) {
-        super(pPrice,pLevel,Prent,pAnimal_owned,pAnimal_owner);
-        String type = "Park";
-    }
-}
